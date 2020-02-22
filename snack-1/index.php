a762d635d098f80ed3466eab2136fd8ceed1b087
Matteo <matteop18@gmail.com>
2020-02-22 14:51:48 +0100

    snack 1 index

--- snack-1/index.php
+++ snack-1/index.php
@@ -12,28 +12,19 @@
   $name = $data['name'];
   $mail = $data['mail'];
   $age = $data['age'];
-  $text = '';
+  $text = 'Completato';
+  echo is_numeric($age);
   // Se name è minore uguale di 3 la variabile text prende il valore accesso negato
   // Con strlen recupero la lunghezza della variabile $name
   if (strlen($name) <= 3) {
     $text = 'Accesso negato';
-  } elseif (!strpos($mail, '@') || !strpos($mail, '.')) {
+  }  elseif (strpos($mail, '@') && strpos($mail, '.')) {
     $text = 'Completato';
-  } elseif (!is_numeric($age)) {
-    $text = 'Completato';
+  } elseif (is_numeric($age)) {
+    $text = 'Accesso riuscito';
   } else {
-    $text = 'Accesso eseguito';
+    // $text = 'Accesso eseguito';
+    $text = 'Accesso negato';
   }
-
+  echo $text;
 ?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <p><?php echo $text; ?></p>
-  </body>
-</html>
